5226b596588ce6cb501ff90b592cd42680cf1bc8
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-23 23:28:24 +0200

    modified css property in footer

--- client/src/ui/Footer.tsx
+++ client/src/ui/Footer.tsx
@@ -17,7 +17,7 @@ function Footer() {
       className={`sticky bottom-0 right-0 flex h-10 w-full bg-white sm:absolute ${isTraining && "hidden"} shadow-inner-strong`}
     >
       <div className={`sm:w-96 ${!user && "hidden"}`} />
-      <div className="mx-10 flex w-full items-center justify-center gap-8 font-patua text-xs text-textPrimary sm:mx-20 sm:justify-between sm:gap-0 sm:text-base lg:text-lg">
+      <div className="mx-10 flex w-full items-center justify-between gap-8 font-patua text-xs text-textPrimary sm:mx-20 sm:justify-between sm:gap-0 sm:text-base lg:text-lg">
         <Link to="/cgu">{t("tos")}</Link>
         <p>{t("aboutUs")}</p>
         <p>Contact</p>
