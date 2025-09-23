fcc7f6d740d6c43538521006f797a7e5eef72f13
NicolasSokolowski <sokolowski.nicolas@gmail.com>
2025-09-23 15:46:42 +0200

    removing useEffect in deck difficulty custom hook

--- client/src/helpers/useDeckDifficultyIndicator.tsx
+++ client/src/helpers/useDeckDifficultyIndicator.tsx
@@ -1,19 +1,14 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 import { useAppSelector } from "../store/hooks";
 
 export function useDeckDifficultyIndicator(deckId: number) {
-  const [deckDifficultyBg, setDeckDifficultyBg] = useState("bg-secondary");
-
   const cards = useAppSelector((state) => state.card.cards);
-  const filteredCards = useMemo(
-    () => cards.filter((c) => c.deck_id === deckId),
-    [cards, deckId]
-  );
 
-  useEffect(() => {
+  return useMemo(() => {
+    const filteredCards = cards.filter((c) => c.deck_id === deckId);
+
     if (filteredCards.length === 0) {
-      setDeckDifficultyBg("bg-secondary");
-      return;
+      return "bg-secondary";
     }
 
     const total = filteredCards.reduce(
@@ -23,14 +18,9 @@ export function useDeckDifficultyIndicator(deckId: number) {
 
     const avg = total / filteredCards.length;
 
-    let bg = "bg-secondary";
-    if (avg === 0) bg = "bg-secondary";
-    else if (avg <= 15) bg = "bg-red-600";
-    else if (avg <= 30) bg = "bg-orange-400";
-    else bg = "bg-green-500";
-
-    setDeckDifficultyBg(bg);
-  }, [filteredCards]);
-
-  return deckDifficultyBg;
+    if (avg === 0) return "bg-secondary";
+    if (avg <= 15) return "bg-red-600";
+    if (avg <= 30) return "bg-orange-400";
+    return "bg-green-500";
+  }, [cards, deckId]);
 }
